98f719ce1b647e319ad28878c5ddc218ebccdad1
James Hall <hi@jameshall.xyz>
2023-01-31 00:15:34 -0800

    Got /ping command working

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const discord = require('./discord');
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-    // msg.guild.channels.cache.find(i => i.name === 'announcements').send(annEmbed)
-    const sanebotChannel = c.channels.cache.find(i => i.name === 'sanebot').id;
-    client.channels.cache.get(sanebotChannel).send('Hello here!')
-});
-
-// Log in to Discord with your client's token
-client.login(token);
+discord.sendMessage('hi there');
+discord.sendMessage('second message');
