6cf16daaf1394cd6d696ae997f1aae1548a5618e
James Hall <hi@jameshall.xyz>
2023-01-31 09:41:48 -0800

    Added spam command

---
+++ commands/spam.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('spam')
+		.setDescription('Spams 20 messages to your channel'),
+	async execute(interaction) {
+		await interaction.reply(`Hi! Here's message #1`);
+		for (let i = 2; i <= 20; i++ ) {
+			await interaction.followUp(`Hi! Here's message #${i}`);
+		}
+	},
+};
