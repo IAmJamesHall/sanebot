1fa9982462436912d6e1af36c00bffc035f2a048
James Hall <hi@jameshall.xyz>
2023-02-13 12:27:58 -0800

    Got it working using webhooks

--- discord.js
+++ discord.js
@@ -1,18 +1,20 @@
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require("node:fs");
+const path = require("node:path");
 // Require the necessary discord.js classes
 
-
-
 module.exports.createClient = async () => {
-	const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-	const token = require('./config.json').token;
-	// Create a new client instance
-	const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-	await client.login(token);
-	return client;
-}
-
+  const {
+    Client,
+    Collection,
+    Events,
+    GatewayIntentBits,
+  } = require("discord.js");
+  const token = require("./config.json").token;
+  // Create a new client instance
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+  await client.login(token);
+  return client;
+};
 
 // // basic logging to the sanebot channel
 // module.exports.discordLog = (message, client) => {
@@ -28,54 +30,61 @@ module.exports.createClient = async () => {
 //   });
 // };
 
-// basic logging to the sanebot channel
-//TODO: rewrite this so it can run multiple times (ex above)
-module.exports.discordLog = (message, client) => {
-	//   const sanebotChannel = client.channels.cache.find(
-	// 	(i) => i.name === "sanebot"
-	//   ).id;
-	  // Get the channel by its ID
-	const channel = client.channels.cache.get('1069883537454551100');
+// // basic logging to the sanebot channel
+// //TODO: rewrite this so it can run multiple times (ex above)
+// module.exports.discordLog = (message, client) => {
+//   //   const sanebotChannel = client.channels.cache.find(
+//   // 	(i) => i.name === "sanebot"
+//   //   ).id;
+//   // Get the channel by its ID
+//   const channel = client.channels.cache.get("1069883537454551100");
 
-	// Send the message
-	channel.send('Hello there!');
+//   // Send the message
+//   channel.send("Hello there!");
+// };
 
-  };
+// module.exports.setUpServerCommands = (client) => {
+//   const { Collection, Events } = require("discord.js");
+//   // pull in commands from ./commands folder
+//   client.commands = new Collection();
 
-module.exports.setUpServerCommands = (client) => {
-  const { Collection, Events } = require('discord.js');
-// pull in commands from ./commands folder
-client.commands = new Collection();
+//   const commandsPath = path.join(__dirname, "commands");
+//   const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
+//     // Set a new item in the Collection with the key as the command name and the value as the exported module
+//     if ("data" in command && "execute" in command) {
+//       client.commands.set(command.data.name, command);
+//     } else {
+//       console.log(
+//         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+//       );
+//     }
+//   }
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+//   //listen for commands, then run the required command
+//   client.on(Events.InteractionCreate, async (interaction) => {
+//     const command = interaction.client.commands.get(interaction.commandName);
 
-//listen for commands, then run the required command
-client.on(Events.InteractionCreate, async interaction => {
-	const command = interaction.client.commands.get(interaction.commandName);
+//     if (!command) {
+//       console.error(
+//         `No command matching ${interaction.commandName} was found.`
+//       );
+//       return;
+//     }
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-
-});
-}
+//     try {
+//       await command.execute(interaction);
+//     } catch (error) {
+//       console.error(error);
+//       await interaction.reply({
+//         content: "There was an error while executing this command!",
+//         ephemeral: true,
+//       });
+//     }
+//   });
+// };
