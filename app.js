f5378faa837064e7f288affdfde5eaa7db1f59f7
James Hall <hi@jameshall.xyz>
2023-02-13 15:13:48 -0800

    Got discordLog working again

--- app.js
+++ app.js
@@ -2,25 +2,21 @@ const { createClient, discordLog, setUpServerCommands } = require('./discord');
 const classifyEmail = require("./gpt");
 const { token } = require('./config.json');
 
-const client = createClient();
-const log = message => {
-    console.log(message);
-    discordLog(message, client);
-}
-setUpServerCommands(client);
 
-log('hello');
-log('hi');
+setUpServerCommands();
+
+// discordLog('hello');
+// discordLog('hi again');
 
 
-// const stuff = async () => {
-//     const classification = await classifyEmail(`{
-//     date: [ 'Mon, 28 Nov 2022 22:48:18 +0000' ],
-//     to: [ 'James Hall <hi@jameshall.xyz>' ],
-//     from: [ 'Apex Hosting <support@apexminecrafthosting.com>' ],
-//     subject: [ 'Order Confirmation' ]
-//     }`);
-//     log(`support@apexminecrafthosting.com: ${classification}`)
-// };
+const stuff = async () => {
+    const classification = await classifyEmail(`{
+    date: [ 'Mon, 28 Nov 2022 22:48:18 +0000' ],
+    to: [ 'James Hall <hi@jameshall.xyz>' ],
+    from: [ 'Apex Hosting <support@apexminecrafthosting.com>' ],
+    subject: [ 'Order Confirmation' ]
+    }`);
+    discordLog(`support@apexminecrafthosting.com: ${classification}`)
+};
 
 stuff();
