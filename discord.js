a520eb04cb8f0ff626e436849eaf609611d5ecb0
James Hall <hi@jameshall.xyz>
2023-02-18 00:10:23 -0800

    Cleaned up discord connector

--- discord.js
+++ discord.js
@@ -1,82 +1,81 @@
-//TODO: migrate this function to 'axios'
-// basic logging to the sanebot channel using a webhook
-module.exports.discordLog = (message) => {
-  const request = require("request");
-  const { discordWebhookURL } = require("./config.json");
-  request.post(discordWebhookURL, {
-    json: { content: message },
-    function() {
-      console.log("done");
-    },
-  });
-};
+const axios = require('axios');
+const { discordWebhookURL, token } = require("./config.json");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
-module.exports.setUpServerCommands = () => {
-  const fs = require("node:fs");
-  const path = require("node:path");
-  // Require the necessary discord.js classes
-  const {
-    Client,
-    Collection,
-    Events,
-    GatewayIntentBits,
-  } = require("discord.js");
-  const token = require("./config.json").token;
-  // Create a new client instance
-  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-  client.login(token);
-
-    // When the client is ready, run this code (only once)
-    // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-    client.once(Events.ClientReady, (c) => {
-      console.log(`Discordbot logged in as ${c.user.tag} and listening for commands!`);
-      const sanebotChannel = c.channels.cache.find(
-        (i) => i.name === "sanebot"
-      ).id;
-      c.channels.cache.get(sanebotChannel).send('Discordbot logged in and listening for commands');
+/**
+ * Posts a message to a Discord channel using a webhook.
+ *
+ * @param {string} message The message to send to the channel.
+ */
+function discordLog(message) {
+  axios.post(discordWebhookURL, { content: message })
+    .then(() => {
+      console.log("Message posted to Discord channel");
+    })
+    .catch((error) => {
+      console.error("Error posting message to Discord channel:", error);
     });
+}
 
+/**
+ * Sets up server commands for a Discord bot.
+ */
+function setUpServerCommands() {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-  // pull in commands from ./commands folder
-  client.commands = new Collection();
-
+  // Load commands from the ./commands directory
+  const fs = require("fs");
+  const path = require("path");
   const commandsPath = path.join(__dirname, "commands");
   const commandFiles = fs
     .readdirSync(commandsPath)
     .filter((file) => file.endsWith(".js"));
 
+  client.commands = new Collection();
   for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    const command = require(path.join(commandsPath, file));
     if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
     } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+      console.warn(`Command at ${file} is missing 'data' or 'execute'`);
     }
   }
 
-  //listen for commands, then run the required command
-  client.on(Events.InteractionCreate, async (interaction) => {
-    const command = interaction.client.commands.get(interaction.commandName);
+  client.on("ready", () => {
+    console.log(`Discord bot logged in as ${client.user.tag}`);
+    const sanebotChannel = client.channels.cache.find(
+      (channel) => channel.name === "sanebot"
+    );
+    if (sanebotChannel) {
+      sanebotChannel.send("Discord bot logged in and listening for commands");
+    } else {
+      console.warn("sanebot channel not found");
+    }
+  });
 
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+      return;
+    }
+
+    const command = client.commands.get(interaction.commandName);
     if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
+      console.warn(`Command not found for '${interaction.commandName}'`);
       return;
     }
 
     try {
       await command.execute(interaction);
     } catch (error) {
-      console.error(error);
+      console.error(`Error executing '${interaction.commandName}':`, error);
       await interaction.reply({
-        content: "There was an error while executing this command!",
+        content: "There was an error while executing this command.",
         ephemeral: true,
       });
     }
   });
-};
+
+  client.login(token);
+}
+
+module.exports = { discordLog, setUpServerCommands };
